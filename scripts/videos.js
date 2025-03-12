28102e133ba0237dd9ed939465f346717f13f82b
khayerhossain <khayerhossain62@gmail.com>
2025-03-13 02:42:05 +0600

    videos section complete

--- scripts/videos.js
+++ scripts/videos.js
@@ -9,26 +9,35 @@ const displayVideos = (videos) => {
         // console.log(video)
         const videosCard = document.createElement('div');
         videosCard.innerHTML = `
-<div class="card bg-base-100 h-[350px]  shadow-sm">
-  <figure class="px-10 pt-10">
-    <img
-      src="${video.thumbnail}"
-      alt=""
-      class="rounded-xl" />
-  </figure>
-  <div class="card-body items-center text-center">
-    <h2 class="card-title">${video.title}</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions">
-    </div>
+ <div class="card bg-base-100  lg:h-[350px] ">
+      <figure class=" relative">
+          <img src="${video.thumbnail}" alt="" class=" rounded w-full lg:h-[220px] object-cover" />
+          <p class="text-sm text-white bg-black rounded px-2 absolute bottom-2 right-2">3hrs 56 min ago</p>
+      </figure>
+      <div class="flex gap-2 px-0 py-5">
+          <div class="avatar">
+              <div
+                  class="ring-primary ring-offset-base-100 w-6 h-6 rounded-full ring ring-offset-2 mt-1 ml-4">
+                  <img src="${video.authors[0].profile_picture}" />
+              </div>
+          </div>
+          <div class="intro">
+              <h2 class="text-lg font-semibold">${video.title}</h2>
+              <p class="text-sm flex justify-center items-center text-gray-400">
+              ${video.authors[0].profile_name}
+                  <img class="w-4 h-4 ml-1" src="https://icons8.com/icon/98A4yZTt9abw/verified-badge"
+                      alt="">
+              </p>
+              <p class="text-sm text-gray-400">${video.others.views} views</p>
+          </div>
+      </div>
   </div>
-</div>
 `;
         videosContainer.appendChild(videosCard);
 
     });
 }
-loadVideos()
+// loadVideos()
 
 
 
